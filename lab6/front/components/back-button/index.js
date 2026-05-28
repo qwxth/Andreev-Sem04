71c340000533e8d76d4949095fb978f545cff6de
qwxth_laptop <grmnandreev@gmail.com>
2026-05-28 15:13:37 +0300

    Remove other labs from lab2

--- lab6/front/components/back-button/index.js
+++
@@ -1,6 +0,0 @@
-export class BackButtonComponent {
-    constructor(parent) { this.parent = parent; }
-    getHTML() { return `<button id="back-button" class="btn btn-outline-red mb-4 ms-3 mt-3"><i class="fas fa-arrow-left me-2"></i>На главную</button>`; }
-    addListeners(listener) { document.getElementById("back-button")?.addEventListener("click", listener); }
-    render(listener) { this.parent.insertAdjacentHTML('afterbegin', this.getHTML()); this.addListeners(listener); }
-}
